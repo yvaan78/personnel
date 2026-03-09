2a3c753629087f1209f7d30e4e0d89e1afce998d
yanissml4110 <144224591+yanissml4110@users.noreply.github.com>
2026-03-09 17:35:20 +0100

    GestionPersonnel root

--- Personnel/src/personnel/GestionPersonnel.java
+++ Personnel/src/personnel/GestionPersonnel.java
@@ -17,100 +17,131 @@ import java.util.TreeSet;
 
 public class GestionPersonnel implements Serializable
 {
-	private static final long serialVersionUID = -105283113987886425L;
-	private static GestionPersonnel gestionPersonnel = null;
-	private SortedSet<Ligue> ligues;
-	private Employe root = new Employe(this, null, "root", "", "", "toor");
-	public final static int SERIALIZATION = 1, JDBC = 2, 
-			TYPE_PASSERELLE = JDBC;  
-	private static Passerelle passerelle = TYPE_PASSERELLE == JDBC ? new jdbc.JDBC() : new serialisation.Serialization();	
-	
-	/**
-	 * Retourne l'unique instance de cette classe.
-	 * Crée cet objet s'il n'existe déjà.
-	 * @return l'unique objet de type {@link GestionPersonnel}.
-	 */
-	
-	public static GestionPersonnel getGestionPersonnel()
-	{
-		if (gestionPersonnel == null)
-		{
-			gestionPersonnel = passerelle.getGestionPersonnel();
-			if (gestionPersonnel == null)
-				gestionPersonnel = new GestionPersonnel();
-		}
-		return gestionPersonnel;
-	}
+    private static final long serialVersionUID = -105283113987886425L;
+    private static GestionPersonnel gestionPersonnel = null;
+    private SortedSet<Ligue> ligues;
+    private Employe root;
+    public final static int SERIALIZATION = 1, JDBC = 2, 
+            TYPE_PASSERELLE = JDBC;  
+    private static Passerelle passerelle = TYPE_PASSERELLE == JDBC ? new jdbc.JDBC() : new serialisation.Serialization();    
+    
+    /**
+     * Retourne l'unique instance de cette classe.
+     * Crée cet objet s'il n'existe déjà.
+     * @return l'unique objet de type {@link GestionPersonnel}.
+     */
+    
+    public static GestionPersonnel getGestionPersonnel()
+    {
+        if (gestionPersonnel == null)
+        {
+            gestionPersonnel = passerelle.getGestionPersonnel();
+            if (gestionPersonnel == null)
+            {
+                gestionPersonnel = new GestionPersonnel();
+                try 
+                {
+                    // Créer le root avec les valeurs par défaut
+                    // et l'insérer automatiquement dans la base
+                    gestionPersonnel.addRoot("root", "toor");
+                } 
+                catch (SauvegardeImpossible e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return gestionPersonnel;
+    }
 
-	public GestionPersonnel()
-	{
-		if (gestionPersonnel != null)
-			throw new RuntimeException("Vous ne pouvez créer qu'une seuls instance de cet objet.");
-		ligues = new TreeSet<>();
-		gestionPersonnel = this;
-	}
-	
-	public void sauvegarder() throws SauvegardeImpossible
-	{
-		passerelle.sauvegarderGestionPersonnel(this);
-	}
-	
-	/**
-	 * Retourne la ligue dont administrateur est l'administrateur,
-	 * null s'il n'est pas un administrateur.
-	 * @param administrateur l'administrateur de la ligue recherchée.
-	 * @return la ligue dont administrateur est l'administrateur.
-	 */
-	
-	public Ligue getLigue(Employe administrateur)
-	{
-		if (administrateur.estAdmin(administrateur.getLigue()))
-			return administrateur.getLigue();
-		else
-			return null;
-	}
+    public GestionPersonnel()
+    {
+        if (gestionPersonnel != null)
+            throw new RuntimeException("Vous ne pouvez créer qu'une seuls instance de cet objet.");
+        ligues = new TreeSet<>();
+        gestionPersonnel = this;
+    }
+    
+    public void sauvegarder() throws SauvegardeImpossible
+    {
+        passerelle.sauvegarderGestionPersonnel(this);
+    }
+    
+    /**
+     * Retourne la ligue dont administrateur est l'administrateur,
+     * null s'il n'est pas un administrateur.
+     * @param administrateur l'administrateur de la ligue recherchée.
+     * @return la ligue dont administrateur est l'administrateur.
+     */
+    
+    public Ligue getLigue(Employe administrateur)
+    {
+        if (administrateur.estAdmin(administrateur.getLigue()))
+            return administrateur.getLigue();
+        else
+            return null;
+    }
 
-	/**
-	 * Retourne toutes les ligues enregistrées.
-	 * @return toutes les ligues enregistrées.
-	 */
-	
-	public SortedSet<Ligue> getLigues()
-	{
-		return Collections.unmodifiableSortedSet(ligues);
-	}
+    /**
+     * Retourne toutes les ligues enregistrées.
+     * @return toutes les ligues enregistrées.
+     */
+    
+    public SortedSet<Ligue> getLigues()
+    {
+        return Collections.unmodifiableSortedSet(ligues);
+    }
 
-	public Ligue addLigue(String nom) throws SauvegardeImpossible
-	{
-		Ligue ligue = new Ligue(this, nom); 
-		ligues.add(ligue);
-		return ligue;
-	}
-	
-	public Ligue addLigue(int id, String nom)
-	{
-		Ligue ligue = new Ligue(this, id, nom);
-		ligues.add(ligue);
-		return ligue;
-	}
+    public Ligue addLigue(String nom) throws SauvegardeImpossible
+    {
+        Ligue ligue = new Ligue(this, nom); 
+        ligues.add(ligue);
+        return ligue;
+    }
+    
+    public Ligue addLigue(int id, String nom)
+    {
+        Ligue ligue = new Ligue(this, id, nom);
+        ligues.add(ligue);
+        return ligue;
+    }
 
-	void remove(Ligue ligue)
-	{
-		ligues.remove(ligue);
-	}
-	
-	int insert(Ligue ligue) throws SauvegardeImpossible
-	{
-		return passerelle.insert(ligue);
-	}
+    void remove(Ligue ligue)
+    {
+        ligues.remove(ligue);
+    }
+    
+    int insert(Ligue ligue) throws SauvegardeImpossible
+    {
+        return passerelle.insert(ligue);
+    }
+    
+    int insert(Employe employe) throws SauvegardeImpossible
+    {
+        return passerelle.insert(employe);
+    }
 
-	/**
-	 * Retourne le root (super-utilisateur).
-	 * @return le root.
-	 */
-	
-	public Employe getRoot()
-	{
-		return root;
-	}
+    /**
+     * Retourne le root (super-utilisateur).
+     * @return le root.
+     */
+    
+    public Employe getRoot()
+    {
+        return root;
+    }
+    
+    /**
+     * Crée le root à partir de son nom et de son mot de passe
+     * et l'affecte à la variable d'instance root.
+     * @param nom le nom du root
+     * @param password le mot de passe du root
+     * @throws SauvegardeImpossible si l'insertion dans la base échoue
+     */
+    public void addRoot(String nom, String password) throws SauvegardeImpossible
+    {
+        // Créer un nouveau root avec les paramètres fournis
+        // Le root n'a pas de ligue (null) et son prénom peut être vide ou "root"
+        this.root = new Employe(this, null, nom, "root", "root@localhost", password);
+    }
 }
