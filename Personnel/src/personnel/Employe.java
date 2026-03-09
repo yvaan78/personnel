eee83cccaa2fcf3c02dfc5847d9072d91f04a2dc
yanissml4110 <144224591+yanissml4110@users.noreply.github.com>
2026-03-09 17:41:12 +0100

    lecture root

--- Personnel/src/personnel/Employe.java
+++ Personnel/src/personnel/Employe.java
@@ -5,10 +5,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Employé d'une ligue hébergée par la M2L. Certains peuvent
+ * Employé d'une ligue hébergée par la M2L. Certains peuvent 
  * être administrateurs des employés de leur ligue.
  * Un seul employé, rattaché à aucune ligue, est le root.
- * Il est impossible d'instancier directement un employé,
+ * Il est impossible d'instancier directement un employé, 
  * il faut passer la méthode {@link Ligue#addEmploye addEmploye}.
  */
 
@@ -20,7 +20,8 @@ public class Employe implements Serializable, Comparable<Employe>
     private LocalDate dateArrivee, dateDepart;
     private Ligue ligue;
     private GestionPersonnel gestionPersonnel;
-   
+    
+    // Constructeur pour insertion (avec auto-incrément)
     Employe(GestionPersonnel gestionPersonnel, Ligue ligue, String nom, String prenom, String mail, String password) throws SauvegardeImpossible
     {
         this.gestionPersonnel = gestionPersonnel;
@@ -31,14 +32,15 @@ public class Employe implements Serializable, Comparable<Employe>
         this.ligue = ligue;
         this.dateArrivee = null;
         this.dateDepart = null;
-       
+        
         // Insertion dans la base de données
         if (gestionPersonnel != null)
         {
             this.id = gestionPersonnel.insert(this);
         }
     }
-   
+    
+    // Constructeur pour reconstruction depuis la base de données (sans insertion)
     Employe(GestionPersonnel gestionPersonnel, Ligue ligue, int id, String nom, String prenom, String mail, String password)
     {
         this.gestionPersonnel = gestionPersonnel;
@@ -51,46 +53,46 @@ public class Employe implements Serializable, Comparable<Employe>
         this.dateArrivee = null;
         this.dateDepart = null;
     }
-   
+    
     public int getId()
     {
         return id;
     }
-   
+    
     public void setId(int id)
     {
         this.id = id;
     }
-   
+    
     /**
-     * Retourne vrai ssi l'employé est administrateur de la ligue
+     * Retourne vrai ssi l'employé est administrateur de la ligue 
      * passée en paramètre.
-     * @return vrai ssi l'employé est administrateur de la ligue
+     * @return vrai ssi l'employé est administrateur de la ligue 
      * passée en paramètre.
-     * @param ligue la ligue pour laquelle on souhaite vérifier si this
+     * @param ligue la ligue pour laquelle on souhaite vérifier si this 
      * est l'admininstrateur.
      */
-   
+    
     public boolean estAdmin(Ligue ligue)
     {
         return ligue.getAdministrateur() == this;
     }
-   
+    
     /**
      * Retourne vrai ssi l'employé est le root.
      * @return vrai ssi l'employé est le root.
      */
-   
+    
     public boolean estRoot()
     {
         return gestionPersonnel.getRoot() == this;
     }
-   
+    
     /**
      * Retourne le nom de l'employé.
-     * @return le nom de l'employé.
+     * @return le nom de l'employé. 
      */
-   
+    
     public String getNom()
     {
         return nom;
@@ -100,7 +102,7 @@ public class Employe implements Serializable, Comparable<Employe>
      * Change le nom de l'employé.
      * @param nom le nouveau nom.
      */
-   
+    
     public void setNom(String nom)
     {
         this.nom = nom;
@@ -110,15 +112,15 @@ public class Employe implements Serializable, Comparable<Employe>
      * Retourne le prénom de l'employé.
      * @return le prénom de l'employé.
      */
-   
+    
     public String getPrenom()
     {
         return prenom;
     }
-   
+    
     /**
      * Change le prénom de l'employé.
-     * @param prenom le nouveau prénom de l'employé.
+     * @param prenom le nouveau prénom de l'employé. 
      */
 
     public void setPrenom(String prenom)
@@ -130,12 +132,12 @@ public class Employe implements Serializable, Comparable<Employe>
      * Retourne le mail de l'employé.
      * @return le mail de l'employé.
      */
-   
+    
     public String getMail()
     {
         return mail;
     }
-   
+    
     /**
      * Change le mail de l'employé.
      * @param mail le nouveau mail de l'employé.
@@ -153,7 +155,7 @@ public class Employe implements Serializable, Comparable<Employe>
      * de l'employé.
      * @param password le password auquel comparer celui de l'employé.
      */
-   
+    
     public boolean checkPassword(String password)
     {
         return this.password.equals(password);
@@ -161,9 +163,9 @@ public class Employe implements Serializable, Comparable<Employe>
 
     /**
      * Change le password de l'employé.
-     * @param password le nouveau password de l'employé.
+     * @param password le nouveau password de l'employé. 
      */
-   
+    
     public void setPassword(String password)
     {
         this.password = password;
@@ -173,18 +175,18 @@ public class Employe implements Serializable, Comparable<Employe>
      * Retourne la date d'arrivée de l'employé.
      * @return la date d'arrivée de l'employé.
      */
-   
+    
     public LocalDate getDateArrivee()
     {
         return dateArrivee;
     }
-   
+    
     /**
      * Change la date d'arrivée de l'employé.
      * @param dateArrivee la nouvelle date d'arrivée.
      * @throws IllegalArgumentException si la date est postérieure à la date de départ
      */
-   
+    
     public void setDateArrivee(LocalDate dateArrivee)
     {
         if (dateDepart != null && dateArrivee != null && dateArrivee.isAfter(dateDepart))
@@ -193,23 +195,23 @@ public class Employe implements Serializable, Comparable<Employe>
         }
         this.dateArrivee = dateArrivee;
     }
-   
+    
     /**
      * Retourne la date de départ de l'employé.
      * @return la date de départ de l'employé.
      */
-   
+    
     public LocalDate getDateDepart()
     {
         return dateDepart;
     }
-   
+    
     /**
      * Change la date de départ de l'employé.
      * @param dateDepart la nouvelle date de départ.
      * @throws IllegalArgumentException si la date est antérieure à la date d'arrivée
      */
-   
+    
     public void setDateDepart(LocalDate dateDepart)
     {
         if (dateArrivee != null && dateDepart != null && dateDepart.isBefore(dateArrivee))
@@ -218,12 +220,12 @@ public class Employe implements Serializable, Comparable<Employe>
         }
         this.dateDepart = dateDepart;
     }
-   
+    
     /**
      * Vérifie si l'employé est actuellement en fonction.
      * @return true si l'employé est en fonction, false sinon
      */
-   
+    
     public boolean estEnFonction()
     {
         LocalDate maintenant = LocalDate.now();
@@ -237,7 +239,7 @@ public class Employe implements Serializable, Comparable<Employe>
      * Retourne la ligue à laquelle l'employé est affecté.
      * @return la ligue à laquelle l'employé est affecté.
      */
-   
+    
     public Ligue getLigue()
     {
         return ligue;
@@ -247,7 +249,7 @@ public class Employe implements Serializable, Comparable<Employe>
      * Supprime l'employé. Si celui-ci est un administrateur, le root
      * récupère les droits d'administration sur sa ligue.
      */
-   
+    
     public void remove()
     {
         Employe root = gestionPersonnel.getRoot();
@@ -260,7 +262,7 @@ public class Employe implements Serializable, Comparable<Employe>
         else
             throw new ImpossibleDeSupprimerRoot();
     }
-   
+    
     @Override
     public int compareTo(Employe autre)
     {
@@ -269,14 +271,14 @@ public class Employe implements Serializable, Comparable<Employe>
             return cmp;
         return getPrenom().compareTo(autre.getPrenom());
     }
-   
+    
     @Override
     public String toString()
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         StringBuilder res = new StringBuilder();
         res.append(nom).append(" ").append(prenom).append(" ").append(mail);
-       
+        
         if (dateArrivee != null)
         {
             res.append(" (arrivée: ").append(dateArrivee.format(formatter));
@@ -286,19 +288,19 @@ public class Employe implements Serializable, Comparable<Employe>
             }
             res.append(")");
         }
-       
+        
         res.append(" (");
         if (estRoot())
             res.append("super-utilisateur");
         else
             res.append(ligue.toString());
         res.append(")");
-       
+        
         if (!estEnFonction() && dateArrivee != null)
         {
             res.append(" [Hors fonction]");
         }
-       
+        
         return res.toString();
     }
 }
